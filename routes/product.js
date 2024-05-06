f239656d431f828064f0048d1fc58044fa44700c
Carlos Zamora <carlos14zamora@gmail.com>
2024-05-06 00:26:14 -0600

    implement GET routes for fetching one or all the products

--- routes/product.js
+++ routes/product.js
@@ -3,12 +3,22 @@ const product = require('../models/product');
 
 router.post('/', (req, res) => {
   const data = req.body;
+
   product.insertMany(data)
-    .then(result => {
-      res.send(result);
-    }).catch(err => {
-      res.status(500).send({ message: err.message });
-    });
+    .then(result => { res.send(result); })
+    .catch(err => { res.status(500).send({ message: err.message }); });
+});
+
+router.get('/', (req, res) => {
+  product.find()
+    .then(result => { res.send(result); })
+    .catch(err => { res.status(500).send({ message: err.message }); });
+});
+
+router.get('/:id', (req, res) => {
+  product.findById(req.params.id)
+    .then(result => { res.send(result); })
+    .catch(err => { res.status(500).send({ message: err.message }); });
 });
 
 module.exports = router;
