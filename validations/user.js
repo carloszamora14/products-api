9c3f5d0df7a3ae1cc9140371e562dee32f85483c
Carlos Zamora <carlos14zamora@gmail.com>
2024-05-10 23:49:32 -0600

    add input validation schemas using joi package

---
+++ validations/user.js
@@ -0,0 +1,14 @@
+const Joi = require('joi');
+
+const authSignup = Joi.object({
+  name: Joi.string().min(3).max(255).required(),
+  email: Joi.string().min(6).max(255).email().required(),
+  password: Joi.string().min(8).max(255).required(),
+});
+
+const authLogin = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+module.exports = { authSignup, authLogin };
