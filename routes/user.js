2aa5ec3e818cd86cb46a6d6e5339e6f0f5adf39e
Carlos Zamora <carlos14zamora@gmail.com>
2024-05-13 23:53:35 -0600

    update signup with password hashing using bcrypt and saving users to mongo database

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const user = require('../models/user');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const { authSignup } = require('../validations/user');
 
 router.post('/signup', async (req, res) => {
@@ -9,7 +10,27 @@ router.post('/signup', async (req, res) => {
     return res.status(400).json({ error: error.details[0].message });
   }
 
-  return res.status(200).json({ message: 'User signed up successfully' });
+  const emailExists = await User.findOne({ email: req.body.email });
+
+  if (emailExists) {
+    return res.status(400).json({ error: 'Email is already registered' });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+  });
+
+  try {
+    const { _id: userId } = await newUser.save();
+    return res.status(200).json({ error: null, data: userId });
+  } catch (err) {
+    return res.status(500).json({ error: 'There was an error' });
+  }
 });
 
 module.exports = router;
